57fe6b399ea5f4c6db140090d8a263e1c3ed08da
Devon Wieczorek <Devon@Wieczoreks-MBP.home>
2019-04-02 16:20:48 -0400

    Added tile styles

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Pagination = () => {
     return(
         <nav aria-label="Page navigation example">
-          <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+          <ul className="pagination">
+            <li className="page-item"><a className="page-link" href="#">&laquo;</a></li>
+            <li className="page-item"><a className="page-link" href="#">1</a></li>
+            <li className="page-item"><a className="page-link" href="#">2</a></li>
+            <li className="page-item"><a className="page-link" href="#">3</a></li>
+            <li className="page-item"><a className="page-link" href="#">&raquo;</a></li>
           </ul>
         </nav>
     );
