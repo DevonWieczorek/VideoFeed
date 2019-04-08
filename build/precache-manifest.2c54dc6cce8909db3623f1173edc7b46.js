08ef1f0f08537a605c63ac0af04c4cac70660951
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-04-08 10:41:47 -0400

    Stop ignoring Build directory
    For Netlify Deployment

---
+++ build/precache-manifest.2c54dc6cce8909db3623f1173edc7b46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78e5e3f93287d64fe46b",
+    "url": "/static/js/main.6ab3acbc.chunk.js"
+  },
+  {
+    "revision": "74e82050e90baba7da46",
+    "url": "/static/js/2.262bd730.chunk.js"
+  },
+  {
+    "revision": "78e5e3f93287d64fe46b",
+    "url": "/static/css/main.1d2df633.chunk.css"
+  },
+  {
+    "revision": "94b31041922951e11a97b3a62d4674c4",
+    "url": "/index.html"
+  }
+];
