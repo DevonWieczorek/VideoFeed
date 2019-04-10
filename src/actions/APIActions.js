95f5e746f1687bd94f27097490de54d16d72a792
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-04-10 10:19:51 -0400

    Renamed BASE_URL to ENDPOINT

--- src/actions/APIActions.js
+++ src/actions/APIActions.js
@@ -1,16 +1,16 @@
-import {BASE_DOMAIN, GET_DATA} from "./types";
+import {ENDPOINT, GET_DATA} from "./types";
 
 export const getData = queryString => {
     return dispatch => {
-        console.log(`Calling ${BASE_DOMAIN}${queryString}...`);
-        fetch(`${BASE_DOMAIN}${queryString}`)
+        console.log(`Calling ${ENDPOINT}${queryString}...`);
+        fetch(`${ENDPOINT}${queryString}`)
             .then(res => res.json())
             .then(res => {
                 dispatch({type: GET_DATA, payload: res});
             })
             .catch(err => {
                 console.log(
-                    `There was an error getting data from ${BASE_DOMAIN}${queryString}:`,
+                    `There was an error getting data from ${ENDPOINT}${queryString}:`,
                     err
                 );
             });
