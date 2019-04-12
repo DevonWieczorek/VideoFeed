7c109517b1d95b4754175c527e40cd10e37b16fb
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-04-12 10:52:01 -0400

    Implemented ErroBoundary in Routing

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Feed from '../pages/Feed';
 import NotFound from '../pages/NotFound';
+import ErrorBoundary from '../components/ErrorBoundary';
 import { Route, Switch } from "react-router-dom";
 
 const Routing = () => {
     return(
         <Switch>
-            <Route exact path="/" key="homeFeed" component={Feed} />
-            <Route path="/:brand" key="brand" component={Feed} />
+            <Route exact path="/" key="homeFeed" component={() => {
+                return(
+                    <ErrorBoundary errorContent={<NotFound/>}>
+                        <Feed />
+                    </ErrorBoundary>
+                );
+            }} />
+            <Route path="/:brand" key="brand" component={() => {
+                return(
+                    <ErrorBoundary errorContent={<NotFound/>}>
+                        <Feed />
+                    </ErrorBoundary>
+                );
+            }} />
             <Route key="404" component={NotFound} />
         </Switch>
     );
