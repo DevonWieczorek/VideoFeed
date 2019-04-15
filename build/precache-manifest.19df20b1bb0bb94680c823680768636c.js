07444962673f49730e1941bdd23c93a65cb93822
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-04-15 16:13:57 -0400

    Fixed dynamic partner logo

--- build/precache-manifest.19df20b1bb0bb94680c823680768636c.js
+++ build/precache-manifest.19df20b1bb0bb94680c823680768636c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "73b33d92d9ec7168e1e5",
-    "url": "/static/js/main.a6903746.chunk.js"
+    "revision": "75e1c97add84bc5c3494",
+    "url": "/static/js/main.b2b1d742.chunk.js"
   },
   {
     "revision": "729136a8be50869ea867",
     "url": "/static/js/2.47750c9f.chunk.js"
   },
   {
-    "revision": "73b33d92d9ec7168e1e5",
+    "revision": "75e1c97add84bc5c3494",
     "url": "/static/css/main.1d2df633.chunk.css"
   },
   {
-    "revision": "76c1e9d491cfe41eec93b28098c33326",
+    "revision": "0be391a4fd558696088f65e667ba7dc5",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cfca870178f196b56030fb0ecba2d767.js"
+  "/precache-manifest.19df20b1bb0bb94680c823680768636c.js"
 );
 
 workbox.clientsClaim();
