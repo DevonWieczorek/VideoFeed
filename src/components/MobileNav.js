1ca79b424556869362032cfb453849d1ac3f6046
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-04-16 15:52:01 -0400

    Added State to MobileNav
    Toggle shut when location changes

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { withRouter } from "react-router-dom";
 import Logo from "./Logo";
 import NavItems from './NavItems';
 
-const MobileNav = () => {
-    return (
-        <nav className="navbar">
+class MobileNav extends Component {
+    state ={isOpen: false}
 
-            <Logo />
+    toggleOpen = (force) => {
+        if(force){
+            this.setState({...this.state, isOpen: force});
+        }
+        else{
+            this.setState({...this.state, isOpen: !this.state.isOpen});
+        }
+    }
 
-            <button className="navbar-toggler border-top border-bottom over-5" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"></button>
+    componentDidUpdate(prevProps) {
+        if (this.props.location !== prevProps.location) {
+            console.log('should toggle');
+            this.toggleOpen(false);
+        }
+    }
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent1">
-                <NavItems />
-            </div>
+    render(){
+        return (
+            <nav className="navbar">
 
-        </nav>
-    );
+                <Logo />
+
+                <button className="navbar-toggler border-top border-bottom over-5" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded={(this.state.isOpen) ? 'true' : 'false'} aria-label="Toggle navigation" onClick={() => {this.toggleOpen()}}></button>
+
+                <div className={`${(this.state.isOpen) ? 'show' : 'collapse'} navbar-collapse`} id="navbarSupportedContent1">
+                    <NavItems />
+                </div>
+
+            </nav>
+        );
+    }
 }
 
-export default MobileNav;
+export default withRouter(MobileNav);
