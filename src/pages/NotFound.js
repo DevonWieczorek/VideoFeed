67798b54c3182d54683faf5e0be31e0010d6f45d
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-04-16 17:05:48 -0400

    Added Search to 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Search from '../components/Search';
 
 const NotFound = () => {
     return(
-        <div>
+        <div className="not-found">
             <h1 className="brand-color">Oops!</h1>
             <div>Looks like we couldn't find what you were looking for.</div>
+            <div className="try-search"><Search /></div>
         </div>
     );
 }
