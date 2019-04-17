8ee948758abd93e49ae2760af303c3111870aed1
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-04-17 12:18:19 -0400

    Made NavItems a stateful component so we can use mapStateToProps

--- src/components/NavItems.js
+++ src/components/NavItems.js
@@ -1,51 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {BrowserView} from "react-device-detect";
-import { NavLink, withRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Search from "./Search";
 import BrandsList from "./BrandsList";
 import CategoriesList from "./CategoriesList";
 import CollapsiblePanel from "./CollapsiblePanel";
 
-const NavItems = () => {
-    return(
-        <>
-            <BrowserView>
+class NavItems extends Component {
+    render(){
+        return(
+            <>
+                <BrowserView>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item border-bottom">
+                            <Search />
+                        </li>
+                    </ul>
+                </BrowserView>
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item border-bottom">
-                        <Search />
+                        <NavLink to="/" className="nav-link truncate" activeClassName="default">
+                            Home Feed
+                        </NavLink>
+                    </li>
+                    <li className="nav-item border-bottom truncate">
+                        <CollapsiblePanel
+                            id="brands-accordion"
+                            title="Brands"
+                        >
+                            <BrandsList />
+                        </CollapsiblePanel>
+                    </li>
+                    <li className="nav-item border-bottom truncate">
+                        <CollapsiblePanel
+                            id="categories-accordion"
+                            title="Categories"
+                        >
+                            <CategoriesList />
+                        </CollapsiblePanel>
                     </li>
                 </ul>
-            </BrowserView>
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item border-bottom">
-                    <NavLink to="/" className="nav-link truncate" activeClassName="default">
-                        Home Feed
-                    </NavLink>
-                </li>
-                <li className="nav-item border-bottom truncate">
-                    <CollapsiblePanel
-                        id="brands-accordion"
-                        title="Brands"
-                    >
-                        <BrandsList />
-                    </CollapsiblePanel>
-                </li>
-                <li className="nav-item border-bottom truncate">
-                    <CollapsiblePanel
-                        id="categories-accordion"
-                        title="Categories"
-                    >
-                        <CategoriesList />
-                    </CollapsiblePanel>
-                </li>
-            </ul>
-        </>
-    );
+            </>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
     return{ ...state.brands};
 }
 
-export default connect(mapStateToProps, {})(withRouter(NavItems));
+export default connect(mapStateToProps, {})(NavItems);
