afa643c63b593ba5940bdd77625764a7b73a169d
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-04-19 10:57:12 -0400

    Made partner logo linkable to home feed of that brand

--- src/components/PartnerLogo.js
+++ src/components/PartnerLogo.js
@@ -1,17 +1,19 @@
 import React, {Component} from "react";
 import {connect} from 'react-redux';
-import { withRouter } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 
 class PartnerLogo extends Component {
     render(){
         return(
             <div className="dynamic-partner-logo">
-                <img
-                    id="partner-logo"
-                    className={this.props.className}
-                    src={this.props.partnerLogo}
-                    alt={this.props.alt || this.props.activeBrand}
-                />
+                <NavLink to={`/${this.props.activeBrand}`} key={this.props.activeBrand}>
+                    <img
+                        id="partner-logo"
+                        className={this.props.className}
+                        src={this.props.partnerLogo}
+                        alt={this.props.alt || this.props.activeBrand}
+                    />
+                </NavLink>
             </div>
         );
     }
